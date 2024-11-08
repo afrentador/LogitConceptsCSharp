4d4105777562afe99236e6b19046594d8f9418d2
Victor Mendoza <vmendozaespina@gmail.com>
2024-11-08 14:59:06 -0500

    Completed exercise '4' determining multiple numbers, and '5' determining leap year

---
+++ LogitConcepts/IsAMultipleOfAnother/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese el Primer Número: ");
+    var b = ConsoleExtension.GetInt("Ingrese el Segundo Número: ");
+  
+
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo de {b}.");
+    }    
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo de {b}.");
+    }
+
+} while (true);
+
+Console.WriteLine("Game Over.");
---
+++ LogitConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,47 @@
+﻿using Shared;
+
+do
+{
+    var currenYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese año: ");
+
+    if (year == currenYear)
+    {
+        message = "es";
+    }
+    else if(year > currenYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {year}, Si {message} biciesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {year}, No {message} biciesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {year}, Si {message} biciesto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {year}, No {message} biciesto.");
+    }
+
+} while (true);
+
+Console.WriteLine("Game Over.");
